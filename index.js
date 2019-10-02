a84848826e7c50f013bc740ca64796df142f0c2d
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-10-01 22:15:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(words, name) {
+    return words.filter(function (word) {
+        return word.toLowerCase() == name.toLowerCase();
+    })
+}
+
+
+function fuzzyMatch(driversList , chr) {
+    return driversList.filter(function (driverName){
+        return driverName[0].toLowerCase() == chr[0].toLowerCase();
+    });
+}
+function matchName(driversList  , name) {
+    return driversList.filter(function(driverName){
+        return driverName.name == name;
+    });
+}
