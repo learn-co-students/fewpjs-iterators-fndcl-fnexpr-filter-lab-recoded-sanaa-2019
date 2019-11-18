aa755f580953a7db8ad42e3a49c219c6c5cd67af
learn <mohammedazizbuss@gmail.com>
2019-11-18 00:47:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+let drivers=[
+    "Mohammed","Ali","Amro"
+]
+let driverss=[
+    {
+        name:"Mohammed",
+        age:"25"
+    }
+    ,{
+        name:"amro",
+        age:"256",
+
+    }
+    ,{
+        name:"Sweet",
+        age:Infinity
+    }
+]
+
+function findMatching(arr,string){
+return arr.filter(ele=>ele.toLowerCase()===string.toLowerCase());
+}
+console.log(findMatching(drivers,"mohammed"));
+
+function fuzzyMatch(arr,string){
+    return arr.filter(ele=>ele.charAt(0).toLowerCase()===string.charAt(0).toLowerCase(0))
+}
+console.log(fuzzyMatch(drivers,"hkram"));
+
+function matchName(arr,string){
+    return arr.filter(function(ele){
+        return  ele.name.toLowerCase()===string.toLowerCase();
+    })
+}
+console.log(matchName(driverss,"Amro"));
