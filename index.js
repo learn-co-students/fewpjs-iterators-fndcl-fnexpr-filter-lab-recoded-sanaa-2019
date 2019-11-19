70738fcee08a97cba80577ac99f48ceaa0ef93f9
EmanAlzariey <eman_alzariey@yahoo.com>
2019-11-19 05:10:42 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(arrDrivers, string){
+
+    return arrDrivers.filter(element => element.toLowerCase() == string.toLowerCase())
+
+}
+
+function fuzzyMatch(arrDrivers , letter) {
+    return arrDrivers.filter( element =>
+         element[0].toLowerCase() == letter[0].toLowerCase()
+    );
+}
+
+function matchName(arrDrivers, string){
+
+    return arrDrivers.filter(element => element.name.toLowerCase() == string.toLowerCase())
+  }
