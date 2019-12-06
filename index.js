86976e17d6f4d8f64e55a187e918739fdd998dc3
Sarah Alafif <sar1997yah@gmail.com>
2019-12-06 19:46:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // Code your solution here
-function fuzzyMatch(driver,string){
-  let name[]=driver.filter(n=>n===string);
-  return name;
+
+function findMatching(drivers, str) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() == str.toLowerCase();
+    })
 }
+
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(function (driver) {
+        return driver[0].toLowerCase() == str[0].toLowerCase();
+    });
+}
+function matchName(drivers, str) {
+    return drivers.filter(function (driver) {
+        return driver.name == str;
+    });
+} 
