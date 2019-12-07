7ec7db2b595bf0e65ceb6b95c572ea6dcd49ace4
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-07 20:19:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(words, name) {
+    return words.filter(function (word) {
+        return word.toLowerCase() == name.toLowerCase();
+    })
+}
+
+
+function fuzzyMatch(drivers , chr) {
+    return drivers.filter(function (driver){
+        return driver[0].toLowerCase() == chr[0].toLowerCase();
+    });
+}
+function matchName(drivers  , name) {
+    return drivers.filter(function(driver){
+        return driver.name == name;
+    });
+}
